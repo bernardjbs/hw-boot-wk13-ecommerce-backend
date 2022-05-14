456791c1afa9ecaa2a189cd611f185a780e44645
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-05-14 23:51:49 +0800

    Added column names on models for tables: category, product, tag, productTag

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,20 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: Datatypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
